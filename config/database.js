c96966159782ba20349a642109f85376abb42e84
Oleksandr Fedoriv <ofedoriv.appwell@ancestry.com>
2025-08-24 21:08:10 +0300

    update

--- config/database.js
+++ config/database.js
@@ -10,5 +10,25 @@ export default {
     dialect: 'postgres',
     logging: console.log,
   },
-  production: config.get('database'),
+  production: {
+    username: config.get('database.username'),
+    password: config.get('database.password'),
+    database: config.get('database.database'),
+    host: config.get('database.host'),
+    port: config.get('database.port'),
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
 };
