8c82333edb0171220ec0651a2cd7702615656ad4
ofedoriv.appwell <ofedoriv.appwell@ancestry.com>
2025-10-02 23:37:41 +0300

    update cron

--- src/cron/rateSourceCron.js
+++ src/cron/rateSourceCron.js
@@ -4,7 +4,6 @@ import { fetchRawRatesFromSources } from '../helpers/fetchRates.js';
 export const startFetchRateSource = () => {
   const isProd = process.env.NODE_ENV !== 'development';
   const cronTimer = isProd ? '0 * * * *' : '30 */2 * * *';
-  // fetchRawRatesFromSources();
 
   cron.schedule(cronTimer, async () => {
     await fetchRawRatesFromSources();
